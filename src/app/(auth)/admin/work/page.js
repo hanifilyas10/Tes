5dc8a079a816806dbe1e014be26b2024c674e959
hanifilyas10 <hanifilyas27@gmail.com>
2024-12-03 20:09:14 +0700

    Porto

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -1,18 +1,31 @@
 "use client"
 import { useState } from 'react';
 import Card from '../../../../components/card';
+import WorkList from './component/work-list'
 
 export default function AdminWork() {
   const [data, setData] = useState({
     title:'',
+    id:'',
     employeType:'',
     companyName:'',
     location:'',
     startDate:'',
     endDate:'',
   });
-  
 
+const clearForm = ()=>{
+  setData({
+    title:'',
+    id:'',
+    employeType:'',
+    companyName:'',
+    location:'',
+    startDate:'',
+    endDate:'',
+  })
+}
+  
   const optEmployeType = [
     {label:'Full Time', value:'full-time'},
     {label:'Part Time', value:'part-time'},
@@ -44,7 +57,41 @@ export default function AdminWork() {
       console.error("ERR", err.message)
       alert(err.message)
     }
-}
+  }
+
+  const onEditItem = async (id)=>{
+    const response = await fetch(`/api/work/${id}`);
+    let resData = await response.json();
+   console.log(resData)
+    setData({
+      id: resData.data[0]._id,
+      title:resData.data[0].title,
+      employeType: resData.data[0].employeType,
+      companyName: resData.data[0].companyName,
+      location: resData.data[0].location,
+      startDate: resData.data[0].startDate,
+      endDate: resData.data[0].endDate,
+    })
+  }
+
+  const onUpdateData = async ()=>{
+    try{
+      let res = await fetch(`/api/work/${data.id}`, {
+        method:'PUT',
+        body: JSON.stringify(data),
+      })
+      let resData = await res.json()
+      if(!resData.data){
+        throw Error(resData.message)
+      }
+      alert("Data berhasil disimpan dengan id")
+      clearForm()
+    }catch(err){
+      console.error("ERR", err.message)
+      alert(err.message)
+    }
+  }
+
 
   return (<>
       <Card title="Work Form" className="pb-5">
@@ -73,12 +120,12 @@ export default function AdminWork() {
             </select>
         </div>
 
-
         <div className="w-full my-2">
             <label>Company Name</label>
             <input 
               name='companyName' 
               type="text" 
+              value={data.companyName}
               onChange={inputHandler}
               className="w-full border my-input-text"/>
         </div>
@@ -102,6 +149,7 @@ export default function AdminWork() {
             <label>Start Date</label>
             <input 
               name='startDate'
+              value={data.startDate}
               onChange={inputHandler}
               type="date" 
               className="w-full border my-input-text"/>
@@ -111,19 +159,38 @@ export default function AdminWork() {
             <label>End Date</label>
             <input 
               name='endDate'
+              value={data.endDate}
               onChange={inputHandler}
               type="date" 
               className="w-full border my-input-text"/>
         </div>
 
-        <button 
-          onClick={onSubmitData}
-          className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
-            <label>Submit Data</label>
-        </button>
-        </Card>
-        <Card title="List of Work" style="mt-5">
-        </Card>
-      </>
-    );
-  }
+            {
+               data.id.length > 0 ?
+                <button 
+                  onClick={onUpdateData}
+                  className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+                    <label>Update Data</label>
+                </button> 
+              :
+                <button 
+                  onClick={onSubmitData}
+                  className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+                    <label>Submit Data</label>
+                </button>
+            }
+
+         <button 
+            onClick={clearForm}
+            className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+              <label>Cancel</label>
+          </button>
+      </Card>
+      
+      <Card title="List of Work" style="mt-5">
+        <WorkList onEditItem={(val)=>onEditItem(val)}/>
+      </Card>
+    </>
+  );
+}
+  
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,24 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-interface ResponseData {
+ 
+interface  ResponseData {
   location: Location | null,
   social: SocialMedia,
-  phone: Phone,
-
+  phone: Phone
 }
 
-interface Location {
+interface  Location {
   country: string;
   city: string;
   street: string;
 }
-interface SocialMedia {
-  email: String;
+
+interface  SocialMedia  {
+  email: string;
   skype: string;
   telegram: string;
 }
-interface Phone {
-  support: string | null;
+
+interface  Phone  {
+  suport: string | null;
   office: string;
   personal: string;
 }
@@ -28,24 +29,22 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-
-  const response : ResponseData={
-    location:{
-      city:"Jakarta",
-      country:"Indonesia",
-      street:"Jl. Banten"
+  const response:ResponseData={
+    location:{ 
+      city:"Jakarta", 
+      country:"Indonesia", 
+      street:"Jl. Jalan"
     },
     phone:{
-      office:"021 555555",
-      personal:"0812 3456 7890",
-      support: null
+      office:"021 88989",
+      personal:"0813 484994 999794", 
+      suport: null
     },
-    social:{
-      email:"myname@gmail.com",
+    social: {
+      email:"myname@mail.com",
       skype:"@myname",
-      telegram:"0812 5645 5555"
+      telegram:"@myname"
     }
   }
   res.status(200).json(response)
 }
-
